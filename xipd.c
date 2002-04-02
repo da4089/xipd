2ab2e50eafeb8d7114fa4cf67733de9fe7064273
David Arnold <d@0x1.org>
2002-04-02 22:31:09 +0000

    more X-ness

--- xipd.c
+++ xipd.c
@@ -1,3 +1,4 @@
+
 #include "config.h"
 
 #if defined(HAVE_ERRNO_H)
@@ -6,10 +7,6 @@
 
 #if defined(HAVE_FCNTL_H)
 # include <fcntl.h>
-#endif
-
-#if defined(HAVE_GETOPT_H)
-# include <getopt.h>
 #endif
 
 #if defined(HAVE_NETDB_H)
@@ -45,19 +42,17 @@
 #endif
 
 #include <X11/Xlib.h>
+#include <X11/Xresource.h>
+#include <X11/Intrinsic.h>
+#include <X11/StringDefs.h>
 
 #include <elvin/elvin.h>
-#include <elvin/memory.h>
-#include <elvin/sync_api.h>
-#include <elvin/errors/elvin.h>
-#include <elvin/errors/unix.h>
-#include <elvin/utils.h>
-
-#include "getopt.h"
+#include <elvin/xt_mainloop.h>
 
 
 /*#define ACTIVE_DELAY  (300)*/
 #define ACTIVE_DELAY  (30)
+#define APP_CLASS     "Xipd"
 
 
 static elvin_error_t error;
@@ -65,39 +60,71 @@ static elvin_handle_t handle;
 static elvin_notification_t nfn;
 
 
+/* Application configuration */
+typedef struct app_data {
+    char *elvin;
+    char *scope;
+    char *user;
+    char *domain;
+    char *location;
+    char *groups;
+    Bool standalone;
+    Bool help;
+    Bool version;
+    Bool debug;
+} *app_data_t;
 
-/* The list of long options */
-static struct option long_options[] =
+/* Legal command-line options */
+static XrmOptionDescRec opt_table[] = 
 {
-    { "elvin", required_argument, NULL, 'e' },
-    { "scope", required_argument, NULL, 'S' },
-    { "user", required_argument, NULL, 'u' },
-    { "domain", required_argument, NULL, 'D' },
-    { "location", required_argument, NULL, 'l' },
-    { "groups", required_argument, NULL, 'g' },
-    { "standalone", no_argument, NULL, 's' },
-    { "display", required_argument, NULL, 'd' },
-    { "version", no_argument, NULL, 'v' },
-    { "help", no_argument, NULL, 'h' },
-    { NULL, no_argument, NULL, '\0' }
+    {"-elvin",       "*elvin",      XrmoptionSepArg, (XPointer)NULL},
+    {"--elvin",      "*elvin",      XrmoptionSepArg, (XPointer)NULL},
+    {"-Scope",       "*scope",      XrmoptionSepArg, (XPointer)NULL},
+    {"--scope",      "*scope",      XrmoptionSepArg, (XPointer)NULL},
+    {"-user",        "*user",       XrmoptionSepArg, (XPointer)NULL},
+    {"--user",       "*user",       XrmoptionSepArg, (XPointer)NULL},
+    {"-Domain",      "*domain",     XrmoptionSepArg, (XPointer)NULL},
+    {"--domain",     "*domain",     XrmoptionSepArg, (XPointer)NULL},
+    {"-location",    "*location",   XrmoptionSepArg, (XPointer)NULL},
+    {"--location",   "*location",   XrmoptionSepArg, (XPointer)NULL},
+    {"-standalone",  "*standalone", XrmoptionNoArg,  "True"},
+    {"--standalone", "*standalone", XrmoptionNoArg,  "True"},
+    {"-help",        "*help",       XrmoptionNoArg,  "True"},
+    {"--help",       "*help",       XrmoptionNoArg,  "True"},
+    {"-version",     "*version",    XrmoptionNoArg,  "True"},
+    {"--version",    "*version",    XrmoptionNoArg,  "True"},
 };
 
 
+/* Conversions and default values for options */
+static XtResource resources[] =
+{
+    {"elvin", "Elvin", XtRString, sizeof(XtRString), XtOffsetOf(struct app_data, elvin), XtRImmediate, NULL},
+    {"scope", "Scope", XtRString, sizeof(XtRString), XtOffsetOf(struct app_data, scope), XtRImmediate, ""},
+    {"user", "User", XtRString, sizeof(XtRString), XtOffsetOf(struct app_data, user), XtRImmediate, ""},
+    {"domain", "Domain", XtRString, sizeof(XtRString), XtOffsetOf(struct app_data, domain), XtRImmediate, ""},
+    {"location", "Location", XtRString, sizeof(XtRString), XtOffsetOf(struct app_data, location), XtRImmediate, ""},
+    {"groups", "Groups", XtRString, sizeof(XtRString), XtOffsetOf(struct app_data, groups), XtRImmediate, ""},
+    {"standalone", "standalone", XtRBool, sizeof(XtRBool), XtOffsetOf(struct app_data, standalone), XtRBool, "False"},
+    {"help", "Help", XtRBool, sizeof(XtRBool), XtOffsetOf(struct app_data, help), XtRBool, "False"},
+    {"version", "Version", XtRBool, sizeof(XtRBool), XtOffsetOf(struct app_data, version), XtRBool, "False"},
+};
+
 
 /* Prints out the usage message */
-static void usage(int argc, char *argv[])
+static void do_usage(int argc, char *argv[], FILE *file)
 {
-    fprintf(stderr, "Usage: %s [OPTION ...]\n", argv[0]);
-    fprintf(stderr, "  -e elvin-url, --elvin=elvin-url\n");
-    fprintf(stderr, "  -S scope,     --scope=scope\n");
-    fprintf(stderr, "  -u user,      --user=user\n");
-    fprintf(stderr, "  -D domain,    --domain=domain\n");
-    fprintf(stderr, "  -l location,  --location=location\n");
-    fprintf(stderr, "  -g groups,    --groups=group[,group...]\n");
-    fprintf(stderr, "  -d display,   --display=host:0\n");
-    fprintf(stderr, "  -s,           --standalone\n");
-    fprintf(stderr, "  -v,           --version\n");
-    fprintf(stderr, "  -h,           --help\n");
+    fprintf(file, "Usage: %s [OPTION ...]\n", argv[0]);
+    fprintf(file, "  -e elvin-url, --elvin=elvin-url\n");
+    fprintf(file, "  -S scope,     --scope=scope\n");
+    fprintf(file, "  -u user,      --user=user\n");
+    fprintf(file, "  -D domain,    --domain=domain\n");
+    fprintf(file, "  -l location,  --location=location\n");
+    fprintf(file, "  -g groups,    --groups=group[,group...]\n");
+    fprintf(file, "  -d display,   --display=host:0\n");
+    fprintf(file, "  -s,           --standalone\n");
+    fprintf(file, "  -v,           --version\n");
+    fprintf(file, "  -h,           --help\n");
 }
 
 
@@ -105,130 +132,8 @@ static void usage(int argc, char *argv[])
 int send_event(int now_active)
 {
     struct timeval tv;
-
-    /* Lookup time */
-    if (gettimeofday(&tv, NULL) < 0) {
-        fprintf(stderr, "Failed to get time of day!\n");
-
-    } else {
-        if (! elvin_notification_add_int32 (nfn, "Date-Time", tv.tv_sec, error)) {
-            elvin_error_fprintf(stderr, error);
-            return 0;
-        }
-    }
-
-    /* Set state */
-    if (! elvin_notification_add_string(nfn, 
-                                        "State", 
-                                        now_active ? "active" : "inactive", 
-                                        error)) {
-        elvin_error_fprintf(stderr, error);
-        return 0;
-    }
-
-    /* Send notification */
-    if (! elvin_sync_notify(handle, nfn, 1, NULL, error)) {
-        elvin_error_fprintf(stderr, error);
-        return 0;
-    }
-
-    /* Clean up notification */
-    elvin_notification_remove(nfn, "Date-Time", NULL);
-    elvin_notification_remove(nfn, "State", NULL);
-
-    return 1;
-}
-
-
-
-int main(int argc, char *argv[])
-{
-    char *display_name = NULL;
-    Display *display;
-    int	getopt_error = 0;
-    char c;
     char host[64], addr[64];
-    char *space, *cr;
     struct hostent *this_host;
-    int is_active = 1;
-    int have_x, have_dpms, have_proc;
-
-    /* Initialise our Elvin error and handle */
-#if ! defined(ELVIN_VERSION_AT_LEAST)
-    if (! (error = elvin_sync_init_default())) {
-        fprintf(stderr, "Failed to initialise Elvin library\n");
-        exit(1);
-    }
-#else
-    if (! (error = elvin_error_alloc(NULL))) {
-        fprintf(stderr, "Failed to allocate error context!\n");
-        exit(1);
-    }
-
-    if (! elvin_sync_init_default(error)) {
-        fprintf(stderr, "Failed to initialise Elvin library\n");
-        exit(1);
-    }
-#endif
-
-    if (! (handle = elvin_handle_alloc(error))) {
-        elvin_error_fprintf(stderr, error);
-        exit(1);
-    }
-
-    /* Process the arguments */
-    optarg = NULL;
-    while (!getopt_error && (-1 != (c = getopt_long(argc, 
-                                                    argv, 
-                                                    "e:S:u:d:l:g:sdvh",
-                                                    long_options,
-                                                    NULL)))) {
-        switch(c) {
-
-        case 'd':
-            display_name = optarg;
-            break;
-
-        case 'e':
-            if (! elvin_handle_append_url(handle, optarg, error)) {
-                elvin_error_fprintf(stderr, error);
-                exit(1);
-            }
-            break;
-
-        case 'S':
-	    if (! elvin_handle_set_discovery_scope(handle, optarg, error)) {
-		elvin_error_fprintf(stderr, error);
-		exit(1);
-	    }
-            break;
-
-        case 'v':
-            fprintf(stdout, VERSION "\n");
-            exit(0);
-            break;
-
-        default:
-            getopt_error++;
-        }
-    }
-
-    if (getopt_error) {
-        usage(argc, argv);
-        exit(1);
-    }
-
-    /* Unlimit connection retries */
-    if (! elvin_handle_set_connection_retries(handle, -1, error)) {
-        elvin_error_fprintf(stderr, error);
-        exit(1);
-    }
-
-    /* Connect to the server */
-    if (! elvin_sync_connect(handle, error)) {
-        elvin_error_fprintf(stderr, error);
-        exit(1);
-    }
 
     /* Build the notification */
     if (! (nfn = elvin_notification_alloc(error))) {
@@ -268,26 +173,50 @@ int main(int argc, char *argv[])
         }
     }
 
+    /* Lookup time */
+    if (gettimeofday(&tv, NULL) < 0) {
+        fprintf(stderr, "Failed to get time of day!\n");
 
-    /* Decide whether we have a useful X display available */
-    have_x = 0;
-
-    if (! display_name) {
-        display_name = getenv("DISPLAY");
+    } else {
+        if (! elvin_notification_add_int32 (nfn, "Date-Time", tv.tv_sec, error)) {
+            elvin_error_fprintf(stderr, error);
+            return 0;
+        }
     }
 
-    if (display_name) {
-        display = XOpenDisplay(display_name);
-        have_x = 1; /* FIXME: error handler? */
+    /* Set state */
+    if (! elvin_notification_add_string(nfn, 
+                                        "State", 
+                                        now_active ? "active" : "inactive", 
+                                        error)) {
+        elvin_error_fprintf(stderr, error);
+        return 0;
     }
 
-    /* Check whether X display supports DPMS */
-    have_dpms = 0;
-    if (have_x) {
-        have_dpms = xipd_have_dpms(display);
+    /* Send notification */
+    if (! elvin_sync_notify(handle, nfn, 1, NULL, error)) {
+        elvin_error_fprintf(stderr, error);
+        return 0;
+    }
+
+    /* Clean up notification */
+    elvin_notification_remove(nfn, "Date-Time", NULL);
+    elvin_notification_remove(nfn, "State", NULL);
+
+    if (! elvin_notification_free(nfn, error)) {
+        elvin_error_fprintf(stderr, error);
+        exit(1);
     }
 
 
+    return 1;
+}
+
+
+int foo(void)
+{
+    Display *display = NULL;
+    int have_x, have_dpms, have_proc, is_active;
 
     /* */
     if (have_dpms) {
@@ -359,19 +288,143 @@ int main(int argc, char *argv[])
             last_mouse = mouse;
         }
     }
+}
 
-    if (! elvin_notification_free(nfn, error)) {
-        elvin_error_fprintf(stderr, error);
-        exit(1);
-    }
+
+void disconnect_cb(elvin_handle_t handle, int result, void *rock, elvin_error_t error)
+{
+    printf("disconnect\n");
+
+    /* Request mainloop be halted. */
+    XtAppSetExitFlag((XtAppContext)rock);
+
+    return;
+}
+
+
+void connect_cb(elvin_handle_t handle, int result, void *rock, elvin_error_t error)
+{
+
+    printf("connect\n");
 
     /* Disconnect and clean up */
-    if (! elvin_sync_disconnect(handle, error) ) {
+    if (! elvin_xt_disconnect(handle, disconnect_cb, rock, error) ) {
         elvin_error_fprintf(stderr, error);
         exit(1);
     }
 
-    if (! elvin_handle_free(handle, error) || ! elvin_sync_cleanup(1, error)) {
+    return;
+}
+
+
+/* Main */
+
+int main(int argc, char *argv[])
+{
+    XtAppContext app_context;
+    Widget toplevel;
+    struct app_data app_data;
+    char *display_name = NULL;
+    int xargc = argc;
+
+    char c;
+    char *space, *cr;
+    int is_active = 1;
+    int have_x, have_dpms, have_proc;
+
+
+    /* Initialize */
+    toplevel = XtVaAppInitialize(&app_context,
+                                 APP_CLASS,
+                                 opt_table,
+                                 16,
+                                 &xargc,
+                                 argv,
+                                 NULL,
+                                 NULL, NULL);
+
+    /* Check for excess args */
+    if (xargc != 1) {
+        do_usage(argc, argv, stderr);
+        exit(1);
+    }
+
+    /* Fetch option values */
+    XtVaGetApplicationResources(toplevel,
+                                &app_data,
+                                resources,
+                                XtNumber(resources),
+                                NULL);
+    /* Help */
+    if (app_data.help == True) {
+        printf("help\n");
+        exit(0);
+    }
+
+    /* Version */
+    if (app_data.version == True) {
+        printf(VERSION "\n");
+        exit(0);
+    }
+
+    /* Check whether we can get activity info */
+    have_dpms = xipd_have_dpms(XtDisplay(toplevel));
+    have_proc = xipd_have_proc();
+
+    if (! have_dpms && ! have_proc) {
+        fprintf(stderr, "Cannot use DPMS or /proc for activity detection.\n");
+        exit(1);
+    }
+
+    /* Initialise Elvin */
+#if ! defined(ELVIN_VERSION_AT_LEAST)
+    if (! (error = elvin_xt_init(app_context))) {
+        fprintf(stderr, "Failed to initialise Elvin library\n");
+        exit(1);
+    }
+#else
+    if (! (error = elvin_error_alloc(NULL))) {
+        fprintf(stderr, "Failed to allocate error context!\n");
+        exit(1);
+    }
+
+    if (! elvin_xt_init_default(app_context, error)) {
+        fprintf(stderr, "Failed to initialise Elvin library\n");
+        exit(1);
+    }
+#endif
+
+    /* Create handle */
+    if (! (handle = elvin_handle_alloc(error))) {
+        elvin_error_fprintf(stderr, error);
+        exit(1);
+    }
+
+    /* Un-limit connection retries */
+    if (! elvin_handle_set_connection_retries(handle, -1, error)) {
+        elvin_error_fprintf(stderr, error);
+        exit(1);
+    }
+
+    /* Add router URL to handle */
+    if (app_data.elvin) {
+        if (! elvin_handle_append_url(handle, app_data.elvin, error)) {
+            elvin_error_fprintf(stderr, error);
+            exit(1);
+        }
+    }
+
+    /* Connect to the server */
+    if (! elvin_xt_connect(handle, connect_cb, (void *)app_context, error)) {
+        elvin_error_fprintf(stderr, error);
+        exit(1);
+    }
+
+    /* Start mainloop */
+    XtAppMainLoop(app_context);
+
+    /* Clean up */
+    if (! elvin_handle_free(handle, error) || ! elvin_xt_cleanup(1, error)) {
         elvin_error_fprintf(stderr, error);
         exit(1);
     }
